38b6a8533ed81afea43a988a4f8bc058ec48c0ff
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-03-30 21:05:53 +0800

    add generate new grid size button

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 
 const sketch_pad = document.querySelector('#sketch_pad');
+const generate_button = document.querySelector('#generate');
+const sizeInput = document.querySelector('#size_input');
+let grid = 16;
 
-let grid = 100, boxCount = grid * grid;
+generateGrid(grid);
 
-generateGrid();
+generate_button.addEventListener('click', (event) => {
+  // event.preventDefault();
+  grid = Number(sizeInput.value);
 
-function generateGrid() {
+  if (grid < 16){
+    sizeInput.value = '16';
+    grid = 16;
+  }else if (grid > 100){
+    sizeInput.value = '100';
+    grid = 100;
+  }
 
+  console.log(grid);
+
+  sketch_pad.innerHTML = '';
+  generateGrid(grid);
+})
+
+function generateGrid(gridSize) {
+  let count = 0;
+  let boxCount = gridSize * gridSize;
   for (let i = 0; i < boxCount; i++) {
 
-    let boxSize = String(600 / grid);
+    let boxSize = String(500 / grid);
     const box = document.createElement("div");
 
     box.classList.add("box");
@@ -21,8 +41,9 @@ function generateGrid() {
       console.log(typeof boxSize === "string");
 
     }
+    count++;
   }
-
+  console.log(count);
 }
 
 
