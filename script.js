6e9360b06a689ccfdbee275e804060f037f5be0c
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-03-31 11:44:37 +0800

    add popup help modal for mode

--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 const sketch_pad = document.querySelector('#sketch_pad');
 const generate_button = document.querySelector('#generate');
 const sizeInput = document.querySelector('#size_input');
+const mode_help_button = document.querySelector('#mode_help_button');
+const help_popup = document.querySelector('#help_popup')
 let grid = 16;
 
 generateGrid(grid);
+
+mode_help_button.addEventListener('mouseenter', () => {
+  help_popup.classList.add('show');
+});
+
+mode_help_button.addEventListener('mouseleave', () => {
+  help_popup.classList.remove('show');
+})
 
 generate_button.addEventListener('click', (event) => {
   // event.preventDefault();
@@ -47,3 +57,6 @@ function generateGrid(gridSize) {
 }
 
 
+//todo
+// add pop up on what does the mode do
+// add a dynamic way to change the size of the sketchpad
